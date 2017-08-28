049ad0184f6b3a932fa5857136aa2616adebf8b7
Brandon Malone <bmmalone@gmail.com>
2017-08-28 17:30:09 +0200

    FMT removed comments code from NaNStandardScaler

--- misc/nan_standard_scaler.py
+++ misc/nan_standard_scaler.py
@@ -20,14 +20,6 @@ class NaNStandardScaler(sklearn.base.TransformerMixin):
         self.col_mean_ = np.nan_to_num(self.col_mean_)
         self.col_std_[self.col_ignore_] = 1
 
-        # make sure anything that is complete missing does not introduce nans
-        #self.col_std_ = np.nan_to_num(self.col_std_)
-
-        # finally, make sure col_std_ does not contain actual 0s
-        #small_val = np.nextafter(0, 1, dtype=X.dtype)
-        #m = self.col_std_ == 0
-        #self.col_std_[m] = small_val
-
         return self
         
     def transform(self, X, *_):
@@ -36,7 +28,6 @@ class NaNStandardScaler(sklearn.base.TransformerMixin):
 
         # so ignore those
         X[:,self.col_ignore_] = 0
-
 
         X = ((X - self.col_mean_) / self.col_std_)
         return X
